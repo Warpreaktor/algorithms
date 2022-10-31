688c688cee6fa8942e62b0580c9bedc3e4d6d11d
warper <aleksey.katachigov@nordclan.com>
2022-10-31 14:52:09 +0300

    - Добавлено решение задачи - "Find Original Array From Doubled Array"

--- src/test/java/ru/warpreaktor/leetcode/FindOriginalArrayFromDoubledArrayTest.java
+++ src/test/java/ru/warpreaktor/leetcode/FindOriginalArrayFromDoubledArrayTest.java
@@ -12,28 +12,28 @@ class FindOriginalArrayFromDoubledArrayTest {
     @Test
     void findOriginalArray() {
         int[] example = new int[]{1, 3, 4, 2, 6, 8};
-//        Assertions.assertArrayEquals(new int[]{4, 3, 1}, FindOriginalArrayFromDoubledArray.findOriginalArray(example));
-//
-//        example = new int[]{6, 3, 0, 1};
-//        Assertions.assertArrayEquals(new int[]{}, FindOriginalArrayFromDoubledArray.findOriginalArray(example));
-//
-//        example = new int[]{6, 3, 0, 0};
-//        Assertions.assertArrayEquals(new int[]{3, 0}, FindOriginalArrayFromDoubledArray.findOriginalArray(example));
-//
-//        example = new int[]{0, 0, 0, 0};
-//        Assertions.assertArrayEquals(new int[]{0, 0}, FindOriginalArrayFromDoubledArray.findOriginalArray(example));
-//
-//        example = new int[]{};
-//        Assertions.assertArrayEquals(new int[]{}, FindOriginalArrayFromDoubledArray.findOriginalArray(example));
-//
-//        example = new int[]{4, 4, 16, 20, 8, 8, 2, 10};
-//        Assertions.assertArrayEquals(new int[]{10, 8, 4, 2}, FindOriginalArrayFromDoubledArray.findOriginalArray(example));
-//
-//        example = new int[100000];
-//        for (int i = 0; i < 100000; i++) {
-//            example[i] = i;
-//        }
-//        Assertions.assertArrayEquals(new int[]{}, FindOriginalArrayFromDoubledArray.findOriginalArray(example));
+        Assertions.assertArrayEquals(new int[]{4, 3, 1}, FindOriginalArrayFromDoubledArray.findOriginalArray(example));
+
+        example = new int[]{6, 3, 0, 1};
+        Assertions.assertArrayEquals(new int[]{}, FindOriginalArrayFromDoubledArray.findOriginalArray(example));
+
+        example = new int[]{6, 3, 0, 0};
+        Assertions.assertArrayEquals(new int[]{3, 0}, FindOriginalArrayFromDoubledArray.findOriginalArray(example));
+
+        example = new int[]{0, 0, 0, 0};
+        Assertions.assertArrayEquals(new int[]{0, 0}, FindOriginalArrayFromDoubledArray.findOriginalArray(example));
+
+        example = new int[]{};
+        Assertions.assertArrayEquals(new int[]{}, FindOriginalArrayFromDoubledArray.findOriginalArray(example));
+
+        example = new int[]{4, 4, 16, 20, 8, 8, 2, 10};
+        Assertions.assertArrayEquals(new int[]{10, 8, 4, 2}, FindOriginalArrayFromDoubledArray.findOriginalArray(example));
+
+        example = new int[100000];
+        for (int i = 0; i < 100000; i++) {
+            example[i] = i;
+        }
+        Assertions.assertArrayEquals(new int[]{}, FindOriginalArrayFromDoubledArray.findOriginalArray(example));
 
         example = new int[200000];
         for (int i = 0; i < 200000 - 1; i++) {
@@ -47,28 +47,28 @@ class FindOriginalArrayFromDoubledArrayTest {
     @Test
     void findOriginalArray2() {
         int[] example = new int[]{1, 3, 4, 2, 6, 8};
-//        Assertions.assertArrayEquals(new int[]{1, 3, 4}, FindOriginalArrayFromDoubledArray.findOriginalArray2(example));
-//
-//        example = new int[]{6, 3, 0, 1};
-//        Assertions.assertArrayEquals(new int[]{}, FindOriginalArrayFromDoubledArray.findOriginalArray2(example));
-//
-//        example = new int[]{6, 3, 0, 0};
-//        Assertions.assertArrayEquals(new int[]{0, 3}, FindOriginalArrayFromDoubledArray.findOriginalArray2(example));
-//
-//        example = new int[]{0, 0, 0, 0};
-//        Assertions.assertArrayEquals(new int[]{0, 0}, FindOriginalArrayFromDoubledArray.findOriginalArray2(example));
-//
-//        example = new int[]{};
-//        Assertions.assertArrayEquals(new int[]{}, FindOriginalArrayFromDoubledArray.findOriginalArray2(example));
-//
-//        example = new int[]{4, 4, 16, 20, 8, 8, 2, 10};
-//        Assertions.assertArrayEquals(new int[]{2, 4, 8, 10}, FindOriginalArrayFromDoubledArray.findOriginalArray2(example));
-//
-//        example = new int[10000000];
-//        for (int i = 0; i < 10000000; i++) {
-//            example[i] = 10000000;
-//        }
-//        Assertions.assertArrayEquals(new int[]{}, FindOriginalArrayFromDoubledArray.findOriginalArray2(example));
+        Assertions.assertArrayEquals(new int[]{1, 3, 4}, FindOriginalArrayFromDoubledArray.findOriginalArray2(example));
+
+        example = new int[]{6, 3, 0, 1};
+        Assertions.assertArrayEquals(new int[]{}, FindOriginalArrayFromDoubledArray.findOriginalArray2(example));
+
+        example = new int[]{6, 3, 0, 0};
+        Assertions.assertArrayEquals(new int[]{0, 3}, FindOriginalArrayFromDoubledArray.findOriginalArray2(example));
+
+        example = new int[]{0, 0, 0, 0};
+        Assertions.assertArrayEquals(new int[]{0, 0}, FindOriginalArrayFromDoubledArray.findOriginalArray2(example));
+
+        example = new int[]{};
+        Assertions.assertArrayEquals(new int[]{}, FindOriginalArrayFromDoubledArray.findOriginalArray2(example));
+
+        example = new int[]{4, 4, 16, 20, 8, 8, 2, 10};
+        Assertions.assertArrayEquals(new int[]{2, 4, 8, 10}, FindOriginalArrayFromDoubledArray.findOriginalArray2(example));
+
+        example = new int[10000000];
+        for (int i = 0; i < 10000000; i++) {
+            example[i] = 10000000;
+        }
+        Assertions.assertArrayEquals(new int[]{}, FindOriginalArrayFromDoubledArray.findOriginalArray2(example));
 
         example = new int[200000];
         for (int i = 0; i < 200000 - 1; i++) {
